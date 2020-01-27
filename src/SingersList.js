6da6a5c86206b35402c5904071ebad30c77d033f
jasmeet kaur <jasmeet.bjf@gmail.com>
2020-01-27 11:02:28 -0600

    add mock data

--- src/SingersList.js
+++ src/SingersList.js
@@ -1,11 +1,38 @@
 import React from "react";
-class SingersList extends Component {
-  state = {  }
-  render() { 
-    return ( <div>
-      
-    </div> );
+import SingerDetails from "./Singersdetails";
+import Singers from "./Singers";
+
+class Component extends React.Component {
+  state = {
+    Singers: []
+  };
+
+  componentDidMount() {
+    this.setState({
+      Singers: SingerDetails
+    });
+  }
+
+  voteHandler = singerId => {
+    const voteSinger = this.state.Singers.map(singer => {
+      //get idea from youtube video and also searched on stack overflow
+      if (singerId === singer.id) {
+        return Object.assign({}, singer, {
+          votes: singer.votes + 1
+        });
+      } else {
+        return singer;
+      }
+    });
+
+    this.setState({
+      Singers: voteSinger
+    });
+  };
+  render() {
+    
+    return <div className="ui divided unstackable items main">{Singer}</div>;
   }
 }
- 
-export default SingersList;
+
+export default Component;
---
+++ src/Singersdetails.js
@@ -0,0 +1,38 @@
+const singersDetails = [
+  {
+    id: 1,
+    name: "Neha Kakkar",
+    votes: 20,
+    submitten_due: " March, 2020",
+    imageUrl: require("./neha.jpg"),
+    Submitted_by: require("./cloud.jpg")
+  },
+  {
+    id: 2,
+    name: "Arjit Singh",
+    votes: 18,
+    submitten_due: " March, 2020",
+    imageUrl: require("./arjit.jpg"),
+    Submitted_by: require("./cloud.jpg")
+  },
+  {
+    id: 3,
+    name: "Shreya Ghosal",
+    votes: 12,
+    submitten_due: " March, 2020",
+    imageUrl: require("./shreya.jpg"),
+    Submitted_by: require("./cloud.jpg")
+  },
+  {
+    id: 4,
+    name: "Atif Aslam",
+    votes: 15,
+    submitten_due: " March, 2020",
+    imageUrl: require("./atif.jpg"),
+    Submitted_by: require("./cloud.jpg")
+  },
+  
+    
+];
+
+export default singersDetails;
